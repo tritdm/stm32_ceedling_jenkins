d8fb5c28f782e4a0119a3f0c21a1b3247f7cc77a
tritdm <20520326@gm.uit.edu.vn>
2024-10-23 17:00:01 +0700

    j

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -95,9 +95,9 @@ int main(void)
   while (1)
   {
 	  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
-	  HAL_Delay(500);
+	  HAL_Delay(1000);
 	  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-	  HAL_Delay(500);
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
